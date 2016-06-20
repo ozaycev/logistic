e8c1c30d1fa8915022138b252a64d5736b1eee7c
ozaycev <site.zaycev@gmail.com>
2016-06-20 21:10:30 +0300

    20.06.2016 - SettingsUsers - edit

--- src/main/java/ua/ozaycev/logistic/entity/SettingsUsers.java
+++ src/main/java/ua/ozaycev/logistic/entity/SettingsUsers.java
@@ -1,6 +1,6 @@
-import com.sun.istack.internal.NotNull;
-import ua.ozaycev.logistic.entity.*;
+package ua.ozaycev.logistic.entity;
 
+import com.sun.istack.internal.NotNull;
 import javax.persistence.Table;
 
 import javax.persistence.*;
@@ -9,13 +9,17 @@ import javax.persistence.*;
 @Table(name="settings_users", schema="cash")
 public class SettingsUsers{
 
-
+	private int id;
 	private Users user;
 	private Company company;
 	private Cash cash;
 	private Shops shop;
 	private Store store;
-	
+
+	public SettingsUsers() {
+
+	}
+
 	public SettingsUsers(Users user, Company company, Cash cash, Shops shop, Store store) {
 		super();
 		this.user = user;
@@ -25,6 +29,18 @@ public class SettingsUsers{
 		this.store = store;
 	}
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@OneToOne(targetEntity = Users.class, cascade = {CascadeType.DETACH, CascadeType.REFRESH})
 	@NotNull
 	public Users getUser() {
 		return user;
@@ -34,6 +50,7 @@ public class SettingsUsers{
 		this.user = user;
 	}
 
+	@OneToOne(targetEntity = Company.class, cascade = {CascadeType.DETACH, CascadeType.REFRESH})
 	@NotNull
 	public Company getCompany() {
 		return company;
@@ -43,6 +60,7 @@ public class SettingsUsers{
 		this.company = company;
 	}
 
+	@OneToOne(targetEntity = Cash.class, cascade = {CascadeType.DETACH, CascadeType.REFRESH})
 	@NotNull
 	public Cash getCash() {
 		return cash;
@@ -52,6 +70,7 @@ public class SettingsUsers{
 		this.cash = cash;
 	}
 
+	@OneToOne(targetEntity = Shops.class, cascade = {CascadeType.DETACH, CascadeType.REFRESH})
 	@NotNull
 	public Shops getShop() {
 		return shop;
@@ -61,6 +80,7 @@ public class SettingsUsers{
 		this.shop = shop;
 	}
 
+	@OneToOne(targetEntity = Store.class, cascade = {CascadeType.DETACH, CascadeType.REFRESH})
 	@NotNull
 	public Store getStore() {
 		return store;
